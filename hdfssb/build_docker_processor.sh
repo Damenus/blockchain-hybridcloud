5ca1ca641c337989fe5d7414b694b46a870b8579
Damian Darczuk <darczuk.damian@gmail.com>
2019-07-22 22:54:34 +0200

    WIP

--- hdfssb/build_docker_processor.sh
+++ hdfssb/build_docker_processor.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-docker-compose -f ../docker-swarm.yml down
+#docker-compose -f ../docker-swarm.yml down
 docker build -t damenus/hdfssb-processor:0.3 .
 cat ~/my_password_docker.txt | docker login --username damenus --password-stdin
 docker push damenus/hdfssb-processor:0.3
